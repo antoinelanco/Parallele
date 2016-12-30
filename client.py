cc542ec392e76888908d603833ef2f03c65d013b
antoinelanco <antoinelanco@gmail.com>
2016-12-30 14:09:00 +0000

    v2

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import time
 import binascii
 import RPi.GPIO as GPIO
 
-gplist = [1,2,3,4,5,6,7,8,26,20]
+gplist = [6,12,16,17,4,23,18,22,21,20]
 data = [0,0,0,0,0,0,0,0]
 GPIO.setmode(GPIO.BCM)
 #init data
@@ -25,16 +25,33 @@ def Parallele():
             while a<8:
                 data[a]=str(GPIO.input(gplist[a]))
                 a=a+1
+            if ifZero(data):
+                print "zero"
+                print res
+                sys.exit(0)
             res = res+ArrayBinToChar(data)
             print res
             GPIO.output(gplist[8],GPIO.HIGH)
 
-def ArrayBinToChar(tab):
 
-    res = tab[0]+'b'+tab[1]+tab[2]+tab[3]+tab[4]+tab[5]+tab[6]+tab[7]
+def ArrayBinToChar(tab):
+    print "test"
+    res = tab[0]+tab[1]+tab[2]+tab[3]+tab[4]+tab[5]+tab[6]+tab[7]
+    print res
     n = int(res, 2)
+    print "test"+str(n)
     return binascii.unhexlify('%x' % n)
 
+def ifZero(tab):
+    res = 0
+    i=0
+    while i<8:
+        res = res + int(tab[i])
+        i=i+8
+    if res == 0:
+        return True
+    else:
+        return False
 
 try:
     Parallele()
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import sys
 import time
 import RPi.GPIO as GPIO
 
-gplist = [1,2,3,4,5,6,7,8,20,26]
+gplist = [6,12,16,17,4,23,18,22,19,26]
 GPIO.setmode(GPIO.BCM)
 #init data
 i=0
@@ -21,19 +21,28 @@ def Parallele():
         print 'Error format : "text"'
         GPIO.cleanup()
         sys.exit(0)
+
     count = 0
     while count<len(text):
         data = charToArrayBin(text[count])
         if GPIO.input(gplist[9]):
 
             a=1
-            print a
             while a<9:
-                print a
+                print str(a)+" : "+data[a]+" : "+str(gplist[a-1])
                 GPIO.output(gplist[a-1],int (data[a]))
                 a=a+1
             GPIO.output(gplist[8],GPIO.HIGH)
             count=count+1
+    # time.sleep(1)
+    # while 1:
+    #     if GPIO.input(gplist[9]):
+    #         a=0
+    #         while a<8:
+    #             GPIO.output(gplist[a],GPIO.LOW)
+    #             a=a+1
+    #         GPIO.output(gplist[8],GPIO.HIGH)
+    #         sys.exit(0)
 
 
 def charToArrayBin(char):
